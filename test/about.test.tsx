6655f86c79ef9bdce0b6a297d3d49023fb174bac
Lyubov Agulova <lu7623@gmail.com>
2024-07-24 14:50:40 +0300

    Update about.test.tsx

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -6,6 +6,6 @@ describe('About', () => {
     it('Should render author name', () => {
         const { container } = render(<About />);
 
-        expect(container.textContent).toBe('AboutAuthor: Kadushkina Ekaterina');
+        expect(container.textContent).toBe('AboutAuthor: Kadushkina Ekaterina Проверяющий: Lu Agulova');
     });
 });
